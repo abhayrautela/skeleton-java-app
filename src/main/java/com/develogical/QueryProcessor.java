b44b302245d89be782db90d2bacaf409370a43a5
abhayrautela <abhayrautela@gmail.com>
2020-06-24 19:56:47 +0530

    Updated

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -16,6 +16,28 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("what is your name")) {
             response = "Abhay Rautela";
         }
+
+        if (query.toLowerCase().contains("what is 1 plus 6")) {
+            response = "7";
+        }
+
+        if (query.toLowerCase().contains("what is 1 plus 13")) {
+            response = "14";
+        }
+
+        if (query.toLowerCase().contains("which of the following numbers is the largest: 1, 11, 35, 433")) {
+            response = "433";
+        }
+
+        if (query.toLowerCase().contains("what is 5 plus 5")) {
+            response = "10";
+        }
+
+        if (query.toLowerCase().contains("what is 15 plus 15")) {
+            response = "30";
+        }
+
+
         return response;
     }
 }
